20d9beffa77d52d9063e83a76c96c29c88ac7fa5
bing <kingsleylow2013@gmail.com>
2026-09-04 20:41:18 +0800

    bought some followers

--- hello.py
+++ hello.py
@@ -6,6 +6,16 @@ print("========================================")
 username =  "cool_creator"
 bio = "Fun blogger"
 followers = 100
+
+followers += 50
+print("day 1", followers)
+
+followers += 20
+print("day 2", followers)
+
+followers -= 10 
+print("day 3", followers)
+
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
