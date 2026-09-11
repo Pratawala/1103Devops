9fc63da6d64efa1612758acbce2984d6f6c50607
bing <kingsleylow2013@gmail.com>
2026-09-12 03:00:22 +0800

    FINALLY DONE

--- auditor.py
+++ auditor.py
@@ -1,37 +1,38 @@
-#first commit kinda nervous!p
-# increment = 0
+#first commit kinda nervous!
 
 def add_inventory():
     inventory = 0
+    input_value = "0" 
+    ""
     Fe = 0 #failed entry 
-    increment = 0
-    while True:
+    while str(input_value) != "quit":
         try:
-            input_value = int(input(  "Current inventory: " + str(inventory) + "\n" + "how much inventory do you want?:"))
-        
-            if  input_value == "quit":
-                        print("total unit processed:", increment)
+            input_value = input(  "Current inventory: " + str(inventory) + "\n" + "how much inventory do you want?:")
+
+            if  str(input_value) == "quit":
+                        print("total unit processed:", inventory)
                         print("failed entries:", Fe)
                         break 
+
+            else:
+                input_value = int(input_value)
+
             
-            elif input_value + inventory >= 500:
-                print("Inventory cannot exceed 500")
-                Fe += 1
+            if input_value + inventory >= 500:
+                print("------!!!!Inventory cannot exceed 500, OVERSTOCK ALERT!!!------")
+                break
                 
             elif input_value < 0:
-                print("Inventory cannot be negative")
+                print("------Inventory cannot be negative------")
                 Fe += 1
 
             else:
-                increment += input_value
                 inventory += input_value
-                print("Current inventory:", inventory)
 
 
         except(ValueError, TypeError): 
-                print("Invalid input, please enter a digit greater than 0 or type 'quit' to exit")
+                print("------Invalid input, please enter a digit greater than 0 or type 'quit' to exit------")
                 Fe += 1
-                break
 
 
 
