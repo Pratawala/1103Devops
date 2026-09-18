d7cab662e13aa23c6c08e27827992027e609bd86
bing <kingsleylow2013@gmail.com>
2026-09-19 00:25:01 +0800

    added tax function and cleaned up code

--- modularauditor.py
+++ modularauditor.py
@@ -15,22 +15,24 @@ def get_valid_input(inventory):
         try:
             input_value = int(input_value)
 
-            if input_value < 0:
-                print("------Inventory cannot be negative------")
+            if input_value <= 0:
+                print("------Inventory cannot be less than zero------")
+                return False
+
+            elif input_value + inventory > 500:
+                print("------!!!!Inventory cannot exceed 500, OVERSTOCK ALERT!!!------")
                 return False
 
             return input_value
 
         except (ValueError, TypeError):
             print("------Invalid input, please enter a digit greater than 0 or type 'quit' to exit------")
+            return False
 
 
 def process_delivery(current_total, new_value):
     new_total = current_total + new_value
 
-    if new_total >= 500:
-        print("------!!!!Inventory cannot exceed 500, OVERSTOCK ALERT!!!------")
-        return current_total
 
     return new_total
 
@@ -48,9 +50,14 @@ def generate_report(total_units, failed_attempts):
 inventory = 0
 failed_attempts = 0
 total_tax = 0
+input_value=0
 
 while True:
+    if input_value!= 0:
+        print("taxable amount:$", calculate_tax(input_value))
+
     input_value = get_valid_input(inventory)
+
 
     if input_value == "quit":
         generate_report(inventory, failed_attempts)
@@ -62,9 +69,6 @@ while True:
 
     new_inventory = process_delivery(inventory, input_value)
 
-    if new_inventory == inventory:
-        failed_attempts += 1
-        continue
 
     inventory = new_inventory
     total_tax += calculate_tax(input_value)
