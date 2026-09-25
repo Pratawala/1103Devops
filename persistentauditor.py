63964af667b3e02ce38c4725642740d0f78113df
bing <kingsleylow2013@gmail.com>
2026-09-25 17:48:23 +0800

    the list is successfully tracking history.

--- persistentauditor.py
+++ persistentauditor.py
@@ -8,7 +8,7 @@ def get_valid_id():
         id_value = input("What is your product ID ?")
 
         if id_value == "quit":
-            return "quit"
+            return "quit", failed_attempts_id
 
         try:
             id_value = int(id_value)
@@ -47,6 +47,7 @@ def get_valid_description(description_value):
         except (ValueError, TypeError):
             print("------Please enter a string------")
 
+
 def get_valid_quantity(quantity_value):
     failed_attempts=0 #place it outside the loop, this variable will be returned once function finish
     while True:
@@ -75,21 +76,28 @@ def get_valid_quantity(quantity_value):
 
 
 def process_delivery(current_id,product_description,quantity_value): #collect all 3 values of list, quantity tally done here
+    good_attempts = 0
     for product in inventory: #current product[id,productName,Quantitytoadd]
         if product[0] == current_id: #grabbing the any product that matches
             product[1] = str(product_description) #updating product name (if any)
             product[2] = int(current_product[2]) + int(quantity_value) #adds current amount with new amount
             with open("inventory.json", "w") as f:
                     json.dump(inventory, f, )
-            return #exits loop so we dont clone list by accident 
+            print(current_product)
+            print("\norders successfully saved to orders.txt")
+            good_attempts+=1
+            return good_attempts#exits loop so we dont clone list by accident 
 
-        else:
+    else:
             current_product[1]= str(product_description)
             current_product[2]= int(quantity_value)
             inventory.append(current_product) #add list if no match
             with open("inventory.json", "w") as f:
                 json.dump(inventory, f, )
-            return
+            print(current_product)
+            print("\norder successfully saved to orders.txt")
+            good_attempts+=1
+            return good_attempts
 
 
     
@@ -100,12 +108,13 @@ def calculate_tax(quantity_value):
     return taxable_amount 
 
 
-def generate_report(total_units, failed_attempts,failed_attempts_id):
+def generate_report(failed_id,failed_quantity,total_units,total_good_attempts):
     print("------Inventory Report------")
+    print("Total transaction processed:", total_good_attempts)
     print("Total unit processed:", total_units)
-    print("Failed entries:", failed_attempts + failed_attempts_id)
+    print("Failed entries:", int(failed_id) + int(failed_quantity))
 
-#ask if can use Json 
+#ask if can use Json , yes i can
 def persistence(): 
     try:
         f = open("inventory.json", "r") #variable f will be assigned the file opened with flag -r
@@ -121,12 +130,22 @@ def persistence():
 
         return inventory
         
+def load_inventory(inventory):
+    print("\n------ Current Inventory ------")
 
+    for product in sorted(inventory, key=lambda product: product[0]):
+        print("ID:", product[0],
+              "| Description:", product[1],
+              "| Quantity:", product[2])
 
 
 inventory = persistence() #pulls out the whole Json inventory file to compare
 failed_attempts = 0
 total_tax = 0
+total_good_attempts = 0
+failed_id = 0
+failed_quantity = 0
+total_units = 0
 
 while True:
     current_product = []
@@ -135,16 +154,27 @@ while True:
     # if quantity_value!= 0:
     #     print("taxable amount:$", calculate_tax(quantity_value))
 
-    current_product = get_valid_id()[0] #ignores the failed attempt variable
-    current_id = current_product[0]
-    product_description = get_valid_description (current_product[1])
-    quantity_value = get_valid_quantity(current_product[2])[0] #ignoring failed variable again
-    process_delivery(current_id,product_description,quantity_value) #not editing ID, only send these two
+    load_inventory(inventory)
 
+    return_get_valid_id = get_valid_id() #saved output of function to a list (no need call function eveyrthing you need variable)
+    if return_get_valid_id[0] == "quit": #user can only quit while editing ID
+            # failed_id = return_get_valid_id[1] #add failed attempt from failed_id 
+            # failed_quantity = return_quantity_value[1]
+            generate_report(failed_id,failed_quantity,total_units,total_good_attempts)
+            break 
 
-    if get_valid_id == "quit": #user can only quit while editing ID
-        generate_report(inventory, failed_attempts)
-        break 
+    else: #initate inventory process 
+        current_product = return_get_valid_id[0]
+        current_id = current_product[0]
+        product_description = get_valid_description (current_product[1])
+        return_quantity_value = get_valid_quantity(current_product[2]) #assigning function return to variable 
+        quantity_value = return_quantity_value[0] #ignoring failed variable again
+       
+        total_units += quantity_value # adding this for report
+        delivery_information = process_delivery(current_id,product_description,quantity_value) #not editing ID, only send these twol_good_attempts += delivery_information #saving delivery information to a variable (in case we need to scale)
+        total_good_attempts += int(delivery_information) #currently an integer as only one variable, no neeed to [x]
+
+    
 
   
 
@@ -155,11 +185,12 @@ while True:
     #added the two other function
     #converted the rest of quantity funciton to grep 
 
-    # ----------------------------------------------------------------
-    #to do
     #existing list value should be overwritten, not create a new one!
     #fix whatever is making my code print instead of amend
-    # create a function similar to process_inventory, but records each list as seperate, regarless of ID
-    #integration 
+
+    # ----------------------------------------------------------------
+    #to do
+    #finish up report generation
+    
 
     
